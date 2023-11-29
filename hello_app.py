7bd8ae43a88aabbadc2e54bead68537e27730ac9
Taylor Huston <taylor.a.huston@gmail.com>
2023-11-29 14:13:21 -0500

    Add count.py

--- hello_app.py
+++ hello_app.py
@@ -1,8 +1,21 @@
+from datetime import datetime
 from flask import Flask
+import calendar
+
+
+#DAYNAMES = ["Monday", "Tuesday", "Wednesday","Thursday", "Friday", "Saturday", "Sunday"]
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
-  return "<p>hello, world!</p>"
+
+  now = datetime.now()
+  #dayname = DAYNAMES[now.weekday()]
+  dayname = calendar.day_name[now.weekday()]
+
+  moon_landing = datetime(1969, 7, 20, 20, 17, 40)
+  diff = now - moon_landing
+
+  return f"<p>hello, world! Happy {dayname}. It has been {diff} since the Moon Landing</p>"
