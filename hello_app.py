cb0d8b60a70efd23ab9a37249171f3cc8b3e98d8
Taylor Huston <taylor.a.huston@gmail.com>
2023-11-29 18:20:34 -0500

    Refactor

--- hello_app.py
+++ hello_app.py
@@ -1,21 +1,15 @@
 from datetime import datetime
+
+
 from flask import Flask
-import calendar
 
 
-#DAYNAMES = ["Monday", "Tuesday", "Wednesday","Thursday", "Friday", "Saturday", "Sunday"]
+from LearnEnoughPython.day import dayname, greeting
+
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
-
-  now = datetime.now()
-  #dayname = DAYNAMES[now.weekday()]
-  dayname = calendar.day_name[now.weekday()]
-
-  moon_landing = datetime(1969, 7, 20, 20, 17, 40)
-  diff = now - moon_landing
-
-  return f"<p>hello, world! Happy {dayname}. It has been {diff} since the Moon Landing</p>"
+  return greeting(datetime.now())
